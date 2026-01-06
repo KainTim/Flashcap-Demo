3de69557c4c00fc930fc00efca8981454f4deefb
Tim Kainz <tikaiz@gmx.at>
2026-01-06 22:54:21 +0100

    Initial commit

---
+++ Flashcap-Demo/Main.cs
@@ -0,0 +1,14 @@
+﻿
+using FlashCap;
+
+var device = new CaptureDevices();
+foreach (var descriptor in device.GetDescriptors())
+{
+    Console.WriteLine($"Name: {descriptor.Name}, " +
+                      $"Description: {descriptor.Description}, " +
+                      $"DeviceType: {descriptor.DeviceType}, " +
+                      $"Identity: {descriptor.Identity}" +
+                      $"Characteristics:\n" +
+                      $"\t{string.Join("\n\t",descriptor.Characteristics.Where(x=>x.Height >=1080))}"
+                      );
+}
