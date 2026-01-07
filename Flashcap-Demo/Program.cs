eca4ab0f150c0faa1264ede58884c8ba5749fd5d
tikaiz <tikaiz@gmx.at>
2026-01-07 13:39:38 +0100

    Added Xabe try, produces 0 byte files

--- Flashcap-Demo/Program.cs
+++ Flashcap-Demo/Program.cs
@@ -1,11 +1,12 @@
 ﻿
 using FlashCap;
+using Xabe.FFmpeg;
 
 namespace Flashcap_Demo;
 
 internal static class Program
 {
-    public static void Main(string[] args)
+    public async static Task Main(string[] args)
     {
         var device = new CaptureDevices();
         var deviceDescriptors = device.GetDescriptors()
@@ -20,6 +21,29 @@ internal static class Program
         Console.WriteLine($"Chosen:\n\t" +
                           $"{captureDeviceDescriptor}\n\t" +
                           $"{characteristic}");
+        
+        var cancellationTokenSource = new CancellationTokenSource();
+        var mediaInfo = await FFmpeg.GetMediaInfo(captureDeviceDescriptor.Identity.ToString(), cancellationTokenSource.Token);
+        
+        var outName = Path.Join(Directory.GetCurrentDirectory(),"output");
+
+        var parameters =
+            $"-f v4l2 " +
+            $"-framerate {characteristic.FramesPerSecond} " +
+            $"-video_size {characteristic.Width}x{characteristic.Height} " +
+            $"-input_format mjpeg " +
+            $"-y " +
+            $"-report " +
+            $"-i {captureDeviceDescriptor.Identity} " +
+            $"{outName}.mkv";
+        FFmpeg.Conversions.New()
+            .Start(parameters, cancellationTokenSource.Token);
+        //     .Build();
+        // Console.WriteLine(cli);
+
+        Console.WriteLine(parameters);
+        await Task.Delay(5000);
+        cancellationTokenSource.Cancel();
     }
     
 }
