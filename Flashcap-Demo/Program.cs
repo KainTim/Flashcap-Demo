ca8b8a362a29a23d0c584015e82299596110be99
Tim Kainz <tikaiz@gmx.at>
2026-01-08 00:12:52 +0100

    Added basic window

--- Flashcap-Demo/Program.cs
+++ Flashcap-Demo/Program.cs
@@ -1,5 +1,9 @@
 ﻿
 using FlashCap;
+using Terminal.Gui.App;
+using Terminal.Gui.Configuration;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
 
 namespace Flashcap_Demo;
 
@@ -8,6 +12,9 @@ internal static class Program
     public static async Task Main(string[] args)
     {
         var backgroundDownload = FFmpeg.DownloadBackground();
+        var app = Application.Create();
+        app.Run<MainWindow>();
+        app.Dispose();
         var device = new CaptureDevices();
         var deviceDescriptors = device.GetDescriptors()
             .Where(x=>x.Characteristics.Length > 0)
@@ -28,6 +35,15 @@ internal static class Program
 
         Console.WriteLine("We have both a selected Device and FFmpeg");
         
+        
     }
     
+}
+
+internal class MainWindow : Window
+{
+    public MainWindow()
+    {
+        Title = "CapEd";
+    }
 }
